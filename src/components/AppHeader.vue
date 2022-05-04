b5ace6f763d4422ffb4f0c6697af6ca9ffa5c41f
DennisGambini <dennis.gambini@gmail.com>
2022-05-04 16:00:13 +0200

    app: scaffolding e bozza header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header-container">
+
+        <div class="logo">
+            <img src="../assets/images/dc-logo.png" alt="dc logo">
+        </div>
+        <app-nav/>
+
+    </div>
+</template>
+
+<script>
+import AppNav from './AppNav.vue'
+
+export default {
+    name: 'AppHeader',
+    components:{
+        AppNav
+    }   
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/style/variables';
+
+   .header-container{
+       @include flex-row-around;
+   }
+
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,73 @@
+<template>
+    <nav>
+        <ul>
+            <li v-for="(link, index) in links" :key="index">
+                {{link.name}}
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name: 'AppHeader',
+    data(){
+        return{
+            links:[
+                {
+                    name: 'characters',
+                    active: true
+                },
+                {
+                    name: 'comics',
+                    active: false
+                },
+                {
+                    name: 'movies',
+                    active: false
+                },
+                {
+                    name: 'tv',
+                    active: false
+                },
+                {
+                    name: 'games',
+                    active: false
+                },
+                {
+                    name: 'collectibles',
+                    active: false
+                },
+                {
+                    name: 'videos',
+                    active: false
+                },
+                {
+                    name: 'fans',
+                    active: false
+                },
+                {
+                    name: 'news',
+                    active: false
+                },
+                {
+                    name: 'shop',
+                    active: false
+                },
+            ]
+        }
+    }   
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/variables';
+
+    ul{
+        list-style: none;
+        @include flex-row-between;
+        gap: 20px;
+    }
+
+</style>
